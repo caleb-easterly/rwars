f75d2693378b009221f01ed1920cc430c03c238b
Caleb <caleb.easterly@gmail.com>
2017-04-01 18:09:26 -0500

    map

--- randomForest.R
+++ randomForest.R
@@ -1,17 +1,19 @@
 library(randomForestSRC)
-
+library(ggmap)
 load("small_dat.rda") # data frame of same name
 
+set.seed(1989)
 small_train_ind <- sample(1:nrow(small_dat), 8000)
 small_train <- small_dat[small_train_ind, ]
 small_test <- small_dat[-small_train_ind, ]
 
 mult_reg_tree <- rfsrc(Multivar(srch_destination_longitude, srch_destination_latitude) ~ 
                            user_location_longitude + user_location_latitude + srch_month + 
-                           length_of_stay + srch_adults_cnt + srch_children_cnt + srch_rm_cnt + is_booking, 
+                           length_of_stay + srch_adults_cnt +
+                           srch_children_cnt + srch_rm_cnt + is_booking, 
                             data = small_dat)
 
-minnesota_person <- subset(small_test, user_location_region == "MN")
+minnesota_person <- subset(small_test, user_location_region == "MN")[7, ]
 
 predictions_mn <- predict(mult_reg_tree, newdata = minnesota_person)
 output_values_mn <- data.frame("long" = predictions_mn$regrOutput$srch_destination_longitude$predicted,
@@ -22,9 +24,10 @@ map <- get_map(location='united states', zoom = 4,
                color = "bw")
 
 ggmap(map, extent = 'device') + 
-    geom_point(data = output_valuesmn, aes(x = long, y = lat), size = 3) + 
-    geom_point(aes(x = minnesota_person$user_location_longitude, y = minnesota_person$user_location_latitude, color = "prediction"), size = 3) + 
-    geom_point(aes(x = minnesota_person$srch_destination_longitude, y = minnesota_person$srch_destination_latitude, color = "truth"), size = 3)
+    geom_point(data = output_values_mn, aes(x = long, y = lat, color = "Prediction"), size = 3) + 
+    geom_point(aes(x = minnesota_person$user_location_longitude, y = minnesota_person$user_location_latitude, color = "Location"), size = 3) + 
+    geom_point(aes(x = minnesota_person$srch_destination_longitude, y = minnesota_person$srch_destination_latitude, color = "Truth"), size = 3) + 
+    scale_color_discrete(name = "Color")
 
 
 predictions <- predict(mult_reg_tree, newdata = minnesota_person)
