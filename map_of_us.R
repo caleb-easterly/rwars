46ae6b9dfae359e9be253b076078996956c93a12
Caleb <caleb.easterly@gmail.com>
2017-04-01 17:09:36 -0500

    better map of us

--- map_of_us.R
+++ map_of_us.R
@@ -15,23 +15,24 @@ lonlat <- select(clean_jdd, user_location_longitude, user_location_latitude, src
 lonlat2 <- data.frame(
     "lat" = as.numeric(c(lonlat$user_location_latitude,lonlat$srch_destination_latitude)),
     "lon" = as.numeric(c(lonlat$user_location_longitude,
-                    lonlat$srch_destination_longitude)),
+                         lonlat$srch_destination_longitude)),
     "loctype" = rep(c("usr", "search"), each = nrow(lonlat)),
     "user_id" = as.factor(rep(lonlat$user_id, 2)),
     "lookbook" = as.factor(rep(lonlat$is_booking, 2))
 )
-                
+
 
 
 # qmplot(user_location_longitude, user_location_latitude, data = clean_jdd)
 
-map <- get_map(location='united states', zoom=4, maptype = "watercolor",
+map <- get_map(location=c(-130, 23, -62, 51), zoom = 4, maptype = "watercolor",
+               source = "stamen",
                color = "bw")
 
 ggmap(map, extent = 'device') + 
-    geom_point(aes(x = lon, y = lat, color = loctype),
-               data = lonlat2, size = 2) +
     geom_path(aes(x = lon, y = lat, group = user_id, color = lookbook),
-              data = lonlat2, size = 0.5) + 
-    theme(legend.position = "none")
-    
+              data = lonlat2, size = 0.8) + 
+    geom_point(aes(x = lon, y = lat, color = loctype),
+               data = lonlat2, size = 2, shape = 1) +
+    theme(legend.position = "none") + 
+    scale_color_manual(values = c("dodgerblue", "darkorange", "purple", "black"))
--- randomForest.R
+++ randomForest.R
@@ -1,8 +1,19 @@
 require(randomForestSRC)
 
 load("small_dat.rda") # data frame of same name
+
+small_train_ind <- sample(1:nrow(small_dat), 8000)
+small_train <- small_dat[small_train_ind, ]
+small_test <- small_dat[-small_train_ind, ]
+
 mult_reg_tree <- rfsrc(Multivar(srch_destination_longitude, srch_destination_latitude) ~ 
-                           user_location_longitude + user_location_latitude + 
-                           length_of_stay + srch_adults_cnt + srch_children_cnt + srch_rm_cnt, 
+                           user_location_longitude + user_location_latitude + srch_month + 
+                           length_of_stay + srch_adults_cnt + srch_children_cnt + srch_rm_cnt + is_booking, 
                             data = small_dat)
 
+minnesota_person <- subset(small_test, user_location_region == "MN" &
+                               srch_month == "2" & srch_adults_cnt ==2 )
+
+predictions <- predict(mult_reg_tree, newdata = minnesota_person)
+output_values <- c(predictions$regrOutput$srch_destination_longitude$predicted,
+            predictions$regrOutput$srch_destination_latitude$predicted)
