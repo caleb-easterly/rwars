131055dcef1275dd935754bf49aba3f9ff3067a9
Caleb <caleb.easterly@gmail.com>
2017-04-01 14:12:36 -0500

    made map

--- read_in_Robj.R
+++ read_in_Robj.R
@@ -15,18 +15,25 @@ conv_month_day <- function(month, day){
     days_in_month <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     sum(days_in_month[1:(month - 1)]) + day
 }
+
+
 require(parallel)
 
 nrows_data_raw <- nrow(data_raw)
 
 cl <- makeCluster(3, type = "FORK")
 
-data_raw$srch_day_of2015 <- parSapply(cl, 1:nrows_data_raw, 
+data_raw$srch_day_of2015 <- parSapply(cl, 1:nrows_data_raw,
                                  function(i) conv_month_day(
-                                     as.numeric(str_sub(data_raw$date_time[i], start = 6, end = 7)), 
+                                     as.numeric(str_sub(data_raw$date_time[i], start = 6, end = 7)),
                                      as.numeric(str_sub(data_raw$date_time[i], start = 9, end = 10))
                                  )
 )
+
+get_month <- function(i) {
+    as.numeric(str_sub(data_raw$date_time[i], start = 6, end = 7))
+}
+
 data_raw$srch_month <- parSapply(cl, 1:nrows_data_raw, 
                                  function(i)
                                      as.numeric(str_sub(data_raw$date_time[i], start = 6, end = 7
@@ -40,6 +47,7 @@ dom_data <- subset(dom_data, !user_location_country != "UNITED STATES OF AMERICA
 
 require(dplyr)
 
+### Join the data
 # drops the search IDs that don't have a corresponding entry in dest_raw, and vice versa
 joined_dest_dom_data <- inner_join(dom_data, dest_raw, by = "srch_destination_id")
 joined_dest_dom_data <- as.data.frame(mutate_at(joined_dest_dom_data, vars(srch_destination_latitude, srch_destination_longitude), funs(as.numeric(.))))
